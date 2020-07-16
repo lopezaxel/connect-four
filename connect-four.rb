42e8add451f5e213d192ca45e8a594faca1ce0c9
Axel Lopez <lopezaxel@protonmail.com>
2020-07-16 10:13:01 -0300

    Add #find_available_row and #write_move with tests

--- connect-four.rb
+++ connect-four.rb
@@ -8,4 +8,22 @@ class Gameboard
   def create_board
     Array.new(6) { Array.new(7, "") }
   end
+
+  def find_available_row(column)
+    board.each_index do |row|
+      return row if board[row][column] == ""
+    end
+
+    nil
+  end
+
+  def write_move(column, mark)
+    row = find_available_row(column)
+    return nil if row.nil?
+
+    board[row][column] = mark
+  end
 end
+
+#gameboard = Gameboard.new
+
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -18,25 +18,55 @@ describe "Gameboard" do
     end
   end
 
-  describe "@board" do
-    gameboard = Gameboard.new
-
-    it "@board should be the return array from #create_method" do
-      expect(gameboard.board).to eql(gameboard.create_board)
-    end
-  end
-
-  describe "#write_move" do
-    gameboard = Gameboard.new
-
-  end
-
   describe "#find_available_row" do
     gameboard = Gameboard.new
 
     it "if the column of the bottom row empty return it" do
       row = gameboard.find_available_row(3)
       expect(row).to eql(0)
+    end
+
+    it "return the upper row if bottom column row isn't empty" do
+      gameboard.board[0][2] = "o"
+      row = gameboard.find_available_row(2)
+      expect(row).to eql(1)
+    end
+
+    it "return nil if the column is full" do
+      gameboard.board[0][3] = "o"
+      gameboard.board[1][3] = "o"
+      gameboard.board[2][3] = "o"
+      gameboard.board[3][3] = "o"
+      gameboard.board[4][3] = "o"
+      gameboard.board[5][3] = "o"
+
+      row = gameboard.find_available_row(3)
+      expect(row).to eql(nil)
+    end
+  end
+
+
+  describe "#write_move" do
+    gameboard = Gameboard.new
+
+    it "should write row 0, column 1" do
+      gameboard.write_move(1, "o")
+      expect(gameboard.board[0][1]).to eql("o")
+    end
+
+    it "should write upto row 5, column 4" do
+      gameboard.write_move(4, "o")
+      gameboard.write_move(4, "o")
+      gameboard.write_move(4, "o")
+      gameboard.write_move(4, "o")
+      gameboard.write_move(4, "o")
+      gameboard.write_move(4, "o")
+
+      expect(gameboard.board[5][4]).to eql("o")
+    end
+
+    it "return nil if column is full" do
+      expect(gameboard.write_move(4, "o")).to eql(nil)
     end
   end
 end
